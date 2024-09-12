534b15455754982309f3ba2f6c5662b3aeab711c
Nazmus Sakib <nsakib.miraz@gmail.com>
2024-09-12 21:20:54 +0600

    bundle single product

--- resources/views/frontend/product/bundle_single_product.blade.php
+++ resources/views/frontend/product/bundle_single_product.blade.php
@@ -2,100 +2,115 @@
 @section('title', $title)
 @section('content')
 
-<div class="container-fluid pb-5">
-    <div class="row px-xl-5">
-        <div class="col-lg-5 mb-30">
-            <div id="product-carousel" class="carousel slide" data-ride="carousel">
-                <div class="carousel-inner bg-light">
-                    @foreach($bundle->images as $index => $image)
-                        <div class="carousel-item {{ $index === 0 ? 'active' : '' }}">
-                            <x-image-with-loader class="d-block w-100" src="{{ asset('/images/bundle_product_images/' . $image->image) }}" alt="Image" style="height: 400px; object-fit: cover;"/>
-                        </div>
-                    @endforeach
-                </div>
-                <a class="carousel-control-prev" href="#product-carousel" data-slide="prev">
-                    <i class="fa fa-2x fa-angle-left text-dark"></i>
-                </a>
-                <a class="carousel-control-next" href="#product-carousel" data-slide="next">
-                    <i class="fa fa-2x fa-angle-right text-dark"></i>
-                </a>
-            </div>
-        </div>
+<div class="page-content mt-3">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-xl-10">
+                <div class="product-details-top">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="product-gallery product-gallery-vertical">
+                                <div class="row">
+                                    <figure class="product-main-image">
+                                        <x-image-with-loader src="{{ asset('/images/bundle_product/' . $bundle->feature_image) }}" alt="{{ $bundle->name }}" class="product-image" />
+                                    </figure>
 
-        <div class="col-lg-7 h-auto mb-30">
-            <div class="h-100 bg-light p-30">
-                <h3>{{ $bundle->name }}</h3>
-                <div class="d-flex mb-3">
-                    <div class="text-primary mr-2"></div>
-                </div>
-                <h3 class="font-weight-semi-bold mb-4">
-                    {{ $currency }} {{ $bundle->price }}
-                </h3>
-
-                <p class="mb-4">{!! $bundle->short_description !!}</p>
-
-                <div class="d-flex flex-row flex-wrap">
-                    @foreach($bundleProducts as $bundleProduct)
-                        <div class="p-3">
-                            <x-image-with-loader src="{{ asset('/images/products/' . $bundleProduct->feature_image) }}" alt="{{ $bundleProduct->name }}" class="img-fluid rounded" style="width: 100px; height: auto;" />
-                            <div class="mt-2">
-                                <h5>{{ $bundleProduct->name }}</h5>
-                                <a href="{{ route('product.show', $bundleProduct->slug) }}" class="btn btn-primary btn-sm">View Product</a>
+                                    <div id="product-zoom-gallery" class="product-image-gallery">
+                                        @foreach($bundle->images as $index => $image)
+                                            <a class="product-gallery-item {{ $index == 0 ? 'active' : '' }}" href="#" data-image="{{ asset('/images/bundle_product_images/' . $image->image) }}" data-zoom-image="{{ asset('/images/bundle_product_images/' . $image->image) }}">
+                                                <img src="{{ asset('/images/bundle_product_images/' . $image->image) }}" alt="product image">
+                                            </a>
+                                        @endforeach
+                                    </div>
+                                </div>
                             </div>
                         </div>
-                    @endforeach
-                </div>
 
-                @if($bundle->quantity <= 0)
-                    <div class="text-danger mt-2 mb-2">
-                        This product is currently out of stock.
-                    </div>
-                @endif
+                        <div class="col-md-6">
+                            <div class="product-details">
+                                <h1 class="product-title">{{ $bundle->name }}</h1>
 
-                <div class="d-flex align-items-center mb-4 pt-2">
-                    <div class="input-group quantity mr-3" style="width: 130px;">
-                        <div class="input-group-btn">
-                            <button class="btn btn-primary" id="decrementBtn">
-                                <i class="fa fa-minus"></i>
-                            </button>
+                                <div class="product-price">
+                                    {{ $currency }} {{ $bundle->price }}
+                                </div>
+
+                                <div class="product-content">
+                                    <p>{!! $bundle->short_description !!} </p>
+                                </div>
+
+                                @if($bundle->quantity <= 0)
+                                    <div class="text-danger mt-2 mb-2">
+                                        This bundle is currently out of stock.
+                                    </div>
+                                @endif
+
+                                <div class="details-filter-row details-row-size">
+                                    <label for="qty">Qty:</label>
+                                    <div class="product-details-quantity">
+                                        <input type="number" id="qty" class="form-control quantity-input" value="1" min="1" max="{{ $bundle->quantity }}" step="1" data-decimals="0" required>
+                                    </div>
+                                </div>
+
+                                <div class="product-details-action">
+                                    <a href="#" 
+                                    class="btn-product btn-cart add-to-cart" 
+                                    data-product-id="{{ $bundle->id }}" 
+                                    data-price="{{ $bundle->price }}"
+                                    data-bundle-id="{{ $bundle->id }}"
+                                    @if($bundle->quantity <= 0)
+                                        style="pointer-events: none; opacity: 0.5;" 
+                                        title="Out of stock"
+                                    @endif>
+                                    <span>Add to cart</span>
+                                    </a>
+                                </div>
+                            </div>
                         </div>
-
-                        <input type="text" class="form-control bg-secondary border-0 text-center" value="1" id="quantityInput" readonly min="1">
-                        <input type="hidden" id="maxQuantity" value="{{ $bundle->quantity !== null ? $bundle->quantity : '' }}">
-
-                        <div class="input-group-btn">
-                            <button class="btn btn-primary" id="incrementBtn">
-                                <i class="fa fa-plus"></i>
-                            </button>
-                        </div>
                     </div>
-                    <button class="btn btn-primary px-3 add-to-cart"
-                            data-price="{{ $bundle->price }}" 
-                            data-offer-id="0"
-                            data-bundle-id="{{ $bundle->id }}" 
-                            @if($bundle->quantity > 0)
-                            @else
-                                disabled
-                            @endif>
-                        <i class="fa fa-shopping-cart mr-1"></i> Add To Cart
-                    </button>
                 </div>
             </div>
-        </div>
-    </div>
-    <div class="row px-xl-5">
-        <div class="col">
-            <div class="bg-light p-30">
-                <div class="nav nav-tabs mb-4">
-                    <a class="nav-item nav-link text-dark active" data-toggle="tab" href="#tab-pane-1">Description</a>
-                    <a class="nav-item nav-link text-dark" data-toggle="tab" href="#tab-pane-2">Reviews (0)</a>
-                </div>
-                <div class="tab-content">
-                    <div class="tab-pane fade show active" id="tab-pane-1">
-                        <h4 class="mb-3">Product Description</h4>
-                        <p>{!! $bundle->long_description !!}</p>
+
+            <aside class="col-xl-2 d-md-none d-xl-block">
+                <div class="sidebar sidebar-product">
+                    <div class="widget widget-products">
+                        <h4 class="widget-title">Get {{ $bundleProducts->count() }} extra products</h4>
+
+                        <div class="products">
+                            @foreach($bundleProducts as $bundleProduct)
+                                <div class="product product-sm">
+                                    <figure class="product-media">
+                                        <a href="{{ route('product.show', $bundleProduct->slug) }}">
+                                            <img src="{{ asset('/images/products/' . $bundleProduct->feature_image) }}" alt="{{ $bundleProduct->name }}" class="product-image">
+                                        </a>
+                                    </figure>
+
+                                    <div class="product-body">
+                                        <h5 class="product-title">
+                                            <a href="{{ route('product.show', $bundleProduct->slug) }}">
+                                                {{ $bundleProduct->name }}
+                                            </a>
+                                        </h5>
+                                    </div>
+                                </div>
+                            @endforeach
+                        </div>
+
                     </div>
-                    <div class="tab-pane fade" id="tab-pane-2">
+                </div>
+            </aside>
+
+            <div class="product-details-tab col-lg-12">
+                <ul class="nav nav-pills justify-content-center" role="tablist">
+                    <li class="nav-item">
+                        <a class="nav-link active" id="product-desc-link" data-toggle="tab" href="#product-desc-tab" role="tab" aria-controls="product-desc-tab" aria-selected="true">Description</a>
+                    </li>
+                </ul>
+                <div class="tab-content">
+                    <div class="tab-pane fade show active" id="product-desc-tab" role="tabpanel" aria-labelledby="product-desc-link">
+                        <div class="product-desc-content">
+                            <h3>Product Information</h3>
+                            {!! $bundle->long_description !!}
+                        </div>
                     </div>
                 </div>
             </div>
@@ -110,23 +125,13 @@
 <script>
     $(document).ready(function() {
         var currentValue = 1;
-        $('#decrementBtn').click(function() {
-            if (currentValue > 1) { 
-                currentValue--;
-                $('#quantityInput').val(currentValue);
-            } else {
-                currentValue = 1;
-                $('#quantityInput').val(currentValue);
+        $('#qty').on('input', function() {
+            var maxValue = parseInt($('#qty').attr('max'));
+            if ($(this).val() > maxValue) {
+                $(this).val(maxValue);
+            } else if ($(this).val() < 1) {
+                $(this).val(1);
             }
-        });
-
-        const maxQuantity = parseInt($('#maxQuantity').val());
-        $('#incrementBtn').click(function() {
-            if (currentValue < maxQuantity) {
-                currentValue++; 
-                $('#quantityInput').val(currentValue);
-            }
-            $('#quantityInput').val(currentValue);
         });
     });
 </script>
