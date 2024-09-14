bd80b0b22ce6c448e976e9eeecacb437cf39d999
Nazmus Sakib <nsakib.miraz@gmail.com>
2024-09-14 18:11:09 +0600

    Cart

--- resources/views/frontend/cart.blade.php
+++ resources/views/frontend/cart.blade.php
@@ -2,114 +2,128 @@
 
 @section('content')
 
-<div class="container-fluid">
-    <div class="row px-xl-5">
-        @if(empty($cart))
-            <div class="col-12">
-                <h2 class="section-title position-relative text-uppercase mx-xl-5 mb-4">
-                    <span class="bg-secondary pr-3">No product in cart</span>
-                </h2>
-            </div>
-        @else
-            <div class="col-lg-8 table-responsive mb-5">
-                <table class="table table-light table-borderless table-hover text-center mb-0">
-                    <thead class="thead-dark">
-                        <tr>
-                            <th>Products</th>
-                            <th>Price</th>
-                            <th>Size</th>
-                            <th>Color</th>
-                            <th>Quantity</th>
-                            <th>Total</th>
-                            <th>Remove</th>
-                        </tr>
-                    </thead>
-                    <tbody class="align-middle">
-                        @php
-                            $currency = \App\Models\CompanyDetails::value('currency');
-                        @endphp
+@php
+    $currency = \App\Models\CompanyDetails::value('currency');
+@endphp
 
-                        @foreach ($cart as $item)
-                            @php
-                                $isBundle = isset($item['bundleId']);
-                                $isCampaign = isset($item['campaignId']);
-                                $isBogo = isset($item['bogoId']);
-                                $isSupplier = isset($item['supplierId']);
-                                $entity = $isBundle ? \App\Models\BundleProduct::find($item['bundleId']) : \App\Models\Product::find($item['productId']);
-                                $price = $item['price'];
-
-                                if ($isBundle) {
-                                    $bundle = \App\Models\BundleProduct::find($item['bundleId']);
-                                    $stock = $bundle->quantity ?? 0;
-                                }elseif ($isCampaign) {
-                                    $campaign = \App\Models\CampaignRequestProduct::find($item['campaignId']);
-                                    $stock = $campaign->quantity ?? 0;
-                                }elseif ($isBogo) {
-                                    $bogo = \App\Models\BuyOneGetOne::find($item['bogoId']);
-                                    $stock = $bogo->quantity ?? 0;
-                                } elseif ($isSupplier) {
-                                    $supplierProduct = \App\Models\SupplierStock::where('supplier_id', $item['supplierId'])
-                                                     ->where('product_id', $item['productId'])
-                                                     ->first();
-                                    $stock = $supplierProduct->quantity ?? 0;
-                                } else {
-                                    $stock = $entity->stock->quantity ?? 0;
-                                }
-                            @endphp
-                            <tr data-entity-id="{{ $isBundle ? $entity->id : $entity->id }}" data-entity-type="{{ $isBundle ? 'bundle' : 'product' }}" data-stock="{{ $stock }}">
-                                <td class="align-middle">
-                                    <div class="d-flex align-items-center">
-                                        <x-image-with-loader src="{{ asset('/images/' . ($isBundle ? 'bundle_product' : 'products') . '/' . $entity->feature_image) }}" alt="{{ $entity->name }}" style="width: 50px; height: 50px; object-fit: contain;" />
-                                        <span class="ml-2">{{ $entity->name }}</span>
-                                    </div>
-                                </td>
-                                <td class="align-middle">{{$currency}} {{ number_format($price, 2) }}</td>
-                                <td class="align-middle">{{ $item['size'] }}</td>
-                                <td class="align-middle">{{ $item['color'] }}</td>
-                                <td class="align-middle">
-                                    <div class="input-group quantity mx-auto" style="width: 100px;">
-                                        <div class="input-group-btn">
-                                            <button class="btn btn-sm btn-primary btn-minus">
-                                                <i class="fa fa-minus"></i>
-                                            </button>
-                                        </div>
-                                        <input type="text" class="form-control form-control-sm bg-secondary border-0 text-center quantity-input" value="{{ $item['quantity'] }}" disabled>
-                                        <div class="input-group-btn">
-                                            <button class="btn btn-sm btn-primary btn-plus">
-                                                <i class="fa fa-plus"></i>
-                                            </button>
-                                        </div>
-                                    </div>
-                                </td>
-                                <td class="align-middle total-cell">{{$currency}} {{ number_format($price * $item['quantity'], 2) }}</td>
-                                <td class="align-middle">
-                                    <button class="btn btn-sm btn-danger remove-from-cart" data-entity-id="{{ $entity->id }}" data-cart-index="{{ $loop->index }}">
-                                        <i class="fa fa-times"></i>
-                                    </button>
-                                </td>
+@if(empty($cart))
+<h1 class="title text-center mb-5 mt-4">Cart is empty</h1>
+@else
+<div class="page-content">
+    <div class="cart">
+        <div class="container">
+        <h1 class="title text-center mb-5 mt-4">Shopping Cart</h1>
+            <div class="row">
+                <div class="col-lg-9">
+                    <table class="table table-cart table-mobile">
+                        <thead>
+                            <tr>
+                                <th>Product</th>
+                                <th>Price</th>
+                                <th>Size</th>
+                                <th>Color</th>
+                                <th>Quantity</th>
+                                <th>Total</th>
+                                <th>Action</th>
                             </tr>
-                        @endforeach
-                    </tbody>
-                </table>
-            </div>
-            <div class="col-lg-4">
-                <div class="bg-light p-30 mb-5">
-                    <div class="pt-2">
-                        <div class="d-flex justify-content-between mt-2">
-                            <h5>Total :</h5>
-                            <h5 id="total">{{$currency}} 0.00</h5>
-                        </div>
+                        </thead>
+                        <tbody>
+                            
+                            @foreach ($cart as $item)
+                                @php
+                                    $isBundle = isset($item['bundleId']);
+                                    $isCampaign = isset($item['campaignId']);
+                                    $isBogo = isset($item['bogoId']);
+                                    $isSupplier = isset($item['supplierId']);
+                                    $entity = $isBundle ? \App\Models\BundleProduct::find($item['bundleId']) : \App\Models\Product::find($item['productId']);
+                                    $price = $item['price'];
+
+                                    if ($isBundle) {
+                                        $bundle = \App\Models\BundleProduct::find($item['bundleId']);
+                                        $stock = $bundle->quantity ?? 0;
+                                    } elseif ($isCampaign) {
+                                        $campaign = \App\Models\CampaignRequestProduct::find($item['campaignId']);
+                                        $stock = $campaign->quantity ?? 0;
+                                    } elseif ($isBogo) {
+                                        $bogo = \App\Models\BuyOneGetOne::find($item['bogoId']);
+                                        $stock = $bogo->quantity ?? 0;
+                                    } elseif ($isSupplier) {
+                                        $supplierProduct = \App\Models\SupplierStock::where('supplier_id', $item['supplierId'])
+                                                        ->where('product_id', $item['productId'])
+                                                        ->first();
+                                        $stock = $supplierProduct->quantity ?? 0;
+                                    } else {
+                                        $stock = $entity->stock->quantity ?? 0;
+                                    }
+                                @endphp
+                                <tr data-entity-id="{{ $isBundle ? $entity->id : $entity->id }}" data-entity-type="{{ $isBundle ? 'bundle' : 'product' }}" data-stock="{{ $stock }}">
+                                    <td class="product-col">
+                                        <div class="product">
+                                            <figure class="product-media">
+                                                <a href="#">
+                                                    <x-image-with-loader src="{{ asset('/images/' . ($isBundle ? 'bundle_product' : 'products') . '/' . $entity->feature_image) }}" alt="{{ $entity->name }}"/>
+                                                </a>
+                                            </figure>
+                                            <h3 class="product-title">
+                                                <a>{{ $entity->name }}</a>
+                                            </h3>
+                                        </div>
+                                    </td>
+                                    <td class="price-col">
+                                        {{ $currency }} {{ number_format($price, 2) }}
+                                    </td>
+                                    <td class="price-col">
+                                        {{ $item['size'] }}
+                                    </td>
+                                    <td class="price-col">
+                                        {{ $item['color'] }}
+                                    </td>
+                                    <td class="quantity-col">
+                                        <div class="cart-product-quantity">
+                                            <input type="number" class="form-control" value="{{ $item['quantity'] }}" min="1" max="{{ $stock }}" step="1" data-decimals="0">
+                                        </div>
+                                    </td>
+                                    <td class="total-col">
+                                        {{ $currency }} {{ number_format($price * $item['quantity'], 2) }}
+                                    </td>
+                                    <td class="remove-col">
+                                        <button class="btn-remove remove-from-cart" data-entity-id="{{ $entity->id }}" data-cart-index="{{ $loop->index }}">
+                                            <i class="icon-close"></i>
+                                        </button>
+                                    </td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+                <aside class="col-lg-3">
+                    <div class="summary summary-cart">
+                        <h3 class="summary-title">Cart Total</h3>
+
+                        <table class="table table-summary">
+                            <tbody>
+                    
+                                <tr class="summary-total">
+                                    <td>Total:</td>
+                                    <td id="total">{{$currency}} 0.00</td>
+                                </tr>
+                            </tbody>
+                        </table>
+
                         <form id="checkout-form" action="{{ route('checkout.store') }}" method="POST">
                             @csrf
                             <input type="hidden" name="cart" value="{{ json_encode($cart) }}">
-                            <button type="submit" class="btn btn-block btn-primary font-weight-bold my-3 py-3">Proceed To Checkout</button>
+                            <button type="submit" class="btn btn-outline-primary-2 btn-order btn-block">Proceed To Checkout</button>
                         </form>
                     </div>
-                </div>
+
+                <a href="{{ route('frontend.shop') }}" class="btn btn-outline-dark-2 btn-block mb-3"><span>CONTINUE SHOPPING</span><i class="icon-refresh"></i></a>
+                </aside>
             </div>
-        @endif
+        </div>
     </div>
 </div>
+@endif
 
 @endsection
 
@@ -121,13 +135,13 @@
     function updateCartTotal() {
         let total = 0;
 
-        $('.table tbody tr').each(function() {
-            let priceText = $(this).find('td:eq(1)').text().trim();
+        $('.table-cart tbody tr').each(function() {
+            let priceText = $(this).find('td.price-col').text().trim();
             let price = parseFloat(priceText.replace(/[^0-9.-]+/g, ''));
-            let quantity = parseInt($(this).find('.quantity-input').val());
+            let quantity = parseInt($(this).find('td.quantity-col input').val());
             let rowTotal = price * quantity;
             total += rowTotal;
-            $(this).find('.total-cell').text(currencySymbol + ' ' + rowTotal.toFixed(2));
+            $(this).find('td.total-col').text(currencySymbol + ' ' + rowTotal.toFixed(2));
         });
 
         $('#total').text(currencySymbol + ' ' + total.toFixed(2));
@@ -150,40 +164,23 @@
     $(document).ready(function() {
         updateCartTotal();
 
-        $(document).on('click', '.btn-plus', function() {
-            let input = $(this).closest('.quantity').find('.quantity-input');
-            let currentValue = parseInt(input.val());
-            let row = $(this).closest('tr'); 
+        $(document).on('change', 'td.quantity-col input', function() {
+            let row = $(this).closest('tr');
             let stock = parseInt(row.data('stock'));
-            let newValue = Math.min(currentValue, stock);
-
-            if (newValue <= stock) {
-                input.val(newValue);
-
-                let productId = row.data('entity-id'); 
-                updateLocalStorage(productId, newValue);
-                updateCartTotal();
-                updateHiddenInputCart();
+            let quantity = parseInt($(this).val());
+            if (quantity > stock) {
+                $(this).val(stock);
             }
-        });
-
-
-        $(document).on('click', '.btn-minus', function() {
-            let input = $(this).closest('.quantity').find('.quantity-input');
-            let currentValue = parseInt(input.val());
-            let newValue = Math.max(currentValue, 1); 
-            input.val(newValue); 
-            let productId = $(this).closest('tr').data('entity-id');
-            updateLocalStorage(productId, newValue);
             updateCartTotal();
-            updateHiddenInputCart(); 
+            updateLocalStorage(row.data('entity-id'), quantity);
+            updateHiddenInputCart();
         });
 
         $(document).on('click', '.remove-from-cart', function() {
-            let productId = $(this).data('product-id');
+            let productId = $(this).data('entity-id');
             $(this).closest('tr').remove();
             updateCartTotal();
-            updateHiddenInputCart(); 
+            updateHiddenInputCart();
         });
     });
 </script>
--- resources/views/frontend/wish_list.blade.php
+++ resources/views/frontend/wish_list.blade.php
@@ -8,65 +8,65 @@
     @endphp
 
     @if($products->isEmpty())
-    <h2 class="title text-center mb-5 mt-4">Wishlist is empty</h2>
+    <h1 class="title text-center mb-5 mt-4">Wishlist is empty</h1>
     @else
-    <h2 class="title text-center mb-5 mt-4">Wishlist</h2>
-        <div class="products">
-            <div class="row justify-content-center">
-            @foreach($products as $product)
-                <div class="col-6 col-md-4 col-lg-3">
-                    <div class="product product-2">
-                        <figure class="product-media">
-                            <a href="{{ route('product.show', $product->slug) }}">
-                                <x-image-with-loader src="{{ asset('/images/products/' . $product->feature_image) }}" alt="{{ $product->name }}" class="product-image" />
-                            </a>
+    <h1 class="title text-center mb-5 mt-4">Wishlist</h1>
+    <div class="products">
+        <div class="row justify-content-center">
+        @foreach($products as $product)
+            <div class="col-6 col-md-4 col-lg-3">
+                <div class="product product-2">
+                    <figure class="product-media">
+                        <a href="{{ route('product.show', $product->slug) }}">
+                            <x-image-with-loader src="{{ asset('/images/products/' . $product->feature_image) }}" alt="{{ $product->name }}" class="product-image" />
+                        </a>
 
-                            @if ($product->stock && $product->stock->quantity > 0)
-                                <div class="product-action-vertical">
-                                    <a href="#" class="btn-product-icon btn-wishlist add-to-wishlist" title="Add to wishlist" 
-                                    data-product-id="{{ $product->id }}"
-                                    data-offer-id="{{ $product->offer_id }}"
-                                    data-price="{{ $product->offer_price ?? $product->flash_sell_price ?? $product->price }}"
-                                    data-campaign-id="{{ $product->campaign_id ?? '' }}"></a>
-                                </div>
-
-                                <div class="product-action">
-                                    <a href="#" class="btn-product btn-cart add-to-cart" title="Add to cart" data-product-id="{{ $product->id }}"
-                                    data-offer-id="{{ $product->offer_id }}"
-                                    data-price="{{ $product->offer_price ?? $product->flash_sell_price ?? $product->price }}" data-campaign-id="{{ $product->campaign_id }}"><span>Add to Cart</span></a>
-                                </div>
-                            @else
-                                <span class="product-label label-out-stock">Out of Stock</span>
-                            @endif
-                        </figure>
-
-                        <div class="product-body">
-                            <h3 class="product-title"><a href="{{ route('product.show', $product->slug) }}">{{ $product->name }}</a></h3>
-                            <div class="product-price">
-                                @if(isset($product->offer_price))
-                                    <del>{{ $currency }} {{ number_format($product->price, 2) }}</del>
-                                    {{ $currency }} {{ number_format($product->offer_price, 2) }}
-                                    @php
-                                        $discountPercentage = (($product->price - $product->offer_price) / $product->price) * 100;
-                                    @endphp
-                                    <small>({{ round($discountPercentage, 0) }}% off)</small>
-                                @elseif(isset($product->flash_sell_price))
-                                    <del>{{ $currency }} {{ number_format($product->price, 2) }}</del>
-                                    {{ $currency }} {{ number_format($product->flash_sell_price, 2) }}
-                                    @php
-                                        $discountPercentage = (($product->price - $product->flash_sell_price) / $product->price) * 100;
-                                    @endphp
-                                    <small>({{ round($discountPercentage, 0) }}% off)</small>
-                                @else
-                                    {{ $currency }} {{ number_format($product->price, 2) }}
-                                @endif
+                        @if ($product->stock && $product->stock->quantity > 0)
+                            <div class="product-action-vertical">
+                                <a href="#" class="btn-product-icon btn-wishlist add-to-wishlist" title="Add to wishlist" 
+                                data-product-id="{{ $product->id }}"
+                                data-offer-id="{{ $product->offer_id }}"
+                                data-price="{{ $product->offer_price ?? $product->flash_sell_price ?? $product->price }}"
+                                data-campaign-id="{{ $product->campaign_id ?? '' }}"></a>
                             </div>
+
+                            <div class="product-action">
+                                <a href="#" class="btn-product btn-cart add-to-cart" title="Add to cart" data-product-id="{{ $product->id }}"
+                                data-offer-id="{{ $product->offer_id }}"
+                                data-price="{{ $product->offer_price ?? $product->flash_sell_price ?? $product->price }}" data-campaign-id="{{ $product->campaign_id }}"><span>Add to Cart</span></a>
+                            </div>
+                        @else
+                            <span class="product-label label-out-stock">Out of Stock</span>
+                        @endif
+                    </figure>
+
+                    <div class="product-body">
+                        <h3 class="product-title"><a href="{{ route('product.show', $product->slug) }}">{{ $product->name }}</a></h3>
+                        <div class="product-price">
+                            @if(isset($product->offer_price))
+                                <del>{{ $currency }} {{ number_format($product->price, 2) }}</del>
+                                {{ $currency }} {{ number_format($product->offer_price, 2) }}
+                                @php
+                                    $discountPercentage = (($product->price - $product->offer_price) / $product->price) * 100;
+                                @endphp
+                                <small>({{ round($discountPercentage, 0) }}% off)</small>
+                            @elseif(isset($product->flash_sell_price))
+                                <del>{{ $currency }} {{ number_format($product->price, 2) }}</del>
+                                {{ $currency }} {{ number_format($product->flash_sell_price, 2) }}
+                                @php
+                                    $discountPercentage = (($product->price - $product->flash_sell_price) / $product->price) * 100;
+                                @endphp
+                                <small>({{ round($discountPercentage, 0) }}% off)</small>
+                            @else
+                                {{ $currency }} {{ number_format($product->price, 2) }}
+                            @endif
                         </div>
                     </div>
                 </div>
-            @endforeach
             </div>
+        @endforeach
         </div>
+    </div>
     @endif
 
 </div>
