e1a1471b901d933c1ab2c07889ac80566f4b9f9d
Nazmus Sakib <nsakib.miraz@gmail.com>
2024-09-15 11:01:51 +0600

    login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,34 +2,43 @@
 
 @section('content')
 
-<div class="container-fluid">
-    <h2 class="position-relative text-uppercase text-center mb-4">
-        <span class="bg-secondary pr-3">Login</span>
-    </h2>
-    <div class="d-flex justify-content-center align-items-center" style="min-height: 50px;">
-        <div class="col-lg-5 mb-5">
-            <div class="contact-form bg-light p-30">
-                @if (session('message'))
-                    <div class="alert alert-danger" role="alert">
-                        {{ session('message') }}
-                    </div>
-                @endif
-                <form name="loginForm" id="loginForm" method="POST" action="{{ route('login') }}">
-                    @csrf
-                    <div class="control-group">
-                        <input class="form-control" id="email" name="email" placeholder="Your Email or Phone Number"
-                            value="{{ old('email') }}" required />
-                        <p class="help-block text-danger"></p>
-                    </div>
-                    <div class="control-group">
-                        <input type="password" class="form-control" id="password" name="password" placeholder="Your Password"
-                            required />
-                        <p class="help-block text-danger"></p>
-                    </div>
-                    <div class="text-center">
-                        <button class="btn btn-primary py-2 px-4" type="submit" id="loginButton">Login</button>
-                    </div>
-                </form>
+<div class="container mt-5 mb-5">
+    <div class="form-box">
+        <div class="form-tab">
+            <ul class="nav nav-pills nav-fill" role="tablist">
+                <li class="nav-item">
+                    <a class="nav-link">Sign In</a>
+                </li>  
+            </ul>
+            <div class="tab-content">
+                <div class="tab-pane fade show active">
+                    @if (session('message'))
+                        <div class="alert alert-danger" role="alert">
+                            {{ session('message') }}
+                        </div>
+                    @endif
+                    <form name="loginForm" id="loginForm" method="POST" action="{{ route('login') }}">
+                         @csrf
+                        <div class="form-group mt-2">
+                            <label for="email">Your Email Address or Phone Number *</label>
+                            <input type="email" class="form-control" id="email" name="email" placeholder="john@example.com or +880xxxxxxxxxx" value="{{ old('email') }}" required />
+                            <p class="help-block text-danger"></p>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="password">Password *</label>
+                            <input type="password" class="form-control" id="password" name="password" placeholder="123456" required>
+                            <p class="help-block text-danger"></p>
+                        </div>
+
+                        <div class="form-footer">
+                            <button type="submit" class="btn btn-outline-primary-2" id="loginButton">
+                                <span>Login</span>
+                                <i class="icon-long-arrow-right"></i>
+                            </button>
+                        </div>
+                    </form>
+                </div>
             </div>
         </div>
     </div>
