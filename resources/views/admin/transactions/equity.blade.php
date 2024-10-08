6aa09ade2e5bccf14aff19046ee72cc5c7d166b3
Nazmus Sakib <nsakib.miraz@gmail.com>
2024-10-08 11:17:42 +0600

    Equity

--- resources/views/admin/transactions/equity.blade.php
+++ resources/views/admin/transactions/equity.blade.php
@@ -7,98 +7,93 @@
         <div class="row">
             <div class="col-md-12">
                 <div id="alert-container"></div>
+                <div class="card card-secondary">
 
-                <div class="row well">
-                    <form class="form-horizontal" role="form" method="POST" action="{{ route('admin.equity.filter') }}">
-                        {{ csrf_field() }}
-
-                        <div class="col-md-3">
-                            <label class="label label-primary">Start Date</label>
-                            <input type="date" class="form-control" name="start_date" value="{{ request()->input('start_date') }}">
+                    <div class="card-header">
+                        <h1 class="card-title">Equity</h1>
+                        <div class="card-tools">
+                            <button class="btn btn-lg btn-success" data-toggle="modal" data-target="#chartModal" data-purpose="0">+ Add New Equity</button>
                         </div>
-                        <div class="col-md-3">
-                            <label class="label label-primary">End Date</label>
-                            <input type="date" class="form-control" name="end_date" value="{{ request()->input('end_date') }}">
-                        </div>
-                        <div class="col-md-3">
-                            <label class="label label-primary">Account</label>
-                            <select class="form-control select2" name="account_name">
-                                <option value="">Select Account..</option>
-                                @foreach ($accounts as $account)
-                                    <option value="{{ $account->account_name }}" {{ request()->input('account_name') == $account->account_name ? 'selected' : '' }}>
-                                        {{ $account->account_name }}
-                                    </option>
-                                @endforeach
-                            </select>
-                        </div>
+                    </div>
 
-                        <div class="col-md-2">
-                            <br>
-                            <button type="submit" class="btn btn-primary btn-sm">Search</button>
+                    <div class="card-body">
+                        <div class="row mb-3">
+                            <form class="form-inline" role="form" method="POST" action="{{ route('admin.equity.filter') }}">
+                                {{ csrf_field() }}
+
+                                <div class="form-group mx-sm-3">
+                                    <label class="sr-only">Start Date</label>
+                                    <input type="date" class="form-control" name="start_date" value="{{ request()->input('start_date') }}">
+                                </div>
+
+                                <div class="form-group mx-sm-3">
+                                    <label class="sr-only">End Date</label>
+                                    <input type="date" class="form-control" name="end_date" value="{{ request()->input('end_date') }}">
+                                </div>
+
+                                <div class="form-group mx-sm-3">
+                                    <label class="sr-only">Account</label>
+                                    <select class="form-control select2" name="account_name">
+                                        <option value="">Select Account..</option>
+                                        @foreach ($accounts as $account)
+                                        <option value="{{ $account->account_name }}" {{ request()->input('account_name') == $account->account_name ? 'selected' : '' }}>
+                                            {{ $account->account_name }}
+                                        </option>
+                                        @endforeach
+                                    </select>
+                                </div>
+
+                                <button type="submit" class="btn btn-primary">Search</button>
+                            </form>
                         </div>
-                    </form>
-                </div>
-
-                @component('components.widget')
-                    @slot('title')
-                    Equity
-                            <button class="btn btn-lg btn-success pull-right" data-toggle="modal"
-                                    data-target="#chartModal"
-                                    data-purpose="0">+
-                                Add New
-                            </button>
-                    @endslot
-                    @slot('description')
-
-                    @endslot
-                    @slot('body')
                         @component('components.table')
-                            @slot('tableID')
-                                expenseTBL
-                            @endslot
-                            @slot('head')
-                                <th>ID</th>
-                                <th>Date</th>
-                                <th>Account</th>
-                                <th>Ref</th>
-                                <th>Description</th>
-                                <th>Transaction Type</th>
-                                <th>Payment Type</th>
-                                <th>Gross Amount</th>
-                                <th>Share Holder</th>
-                                <th><i class=""></i> Action</th>
-                            @endslot
+                        @slot('tableID')
+                        expenseTBL
+                        @endslot
+                        @slot('head')
+                        <th>ID</th>
+                        <th>Date</th>
+                        <th>Account</th>
+                        <th>Ref</th>
+                        <th>Description</th>
+                        <th>Transaction Type</th>
+                        <th>Payment Type</th>
+                        <th>Gross Amount</th>
+                        <th>Share Holder</th>
+                        <th><i class=""></i> Action</th>
+                        @endslot
                         @endcomponent
-                    @endslot
-                @endcomponent
+                    </div>
+                </div>
             </div>
         </div>
     </div>
 </section>
 
 <div class="modal fade" id="chartModal">
-    <div class="modal-dialog">
+    <div class="modal-dialog modal-lg">
         <div class="modal-content">
             <div class="modal-header">
+                <h4 class="modal-title">Equity</h4>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span></button>
-                <h4 class="modal-title">Equity</h4>
             </div>
             <form class="form-horizontal" id="customer-form">
                 <div id="alert-container1"></div>
                 <div class="modal-body">
                     {{csrf_field()}}
                     
-                    <div class="form-group">
-                        <label for="date" class="col-sm-3 control-label">Date</label>
-                        <div class="col-sm-9">
-                            <input type="date" name="date" class="form-control " id="date" value="{{date('Y-m-d')}}">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <label for="date" class="control-label">Date</label>
+                                <input type="date" name="date" class="form-control " id="date" value="{{date('Y-m-d')}}">
+                            </div>
                         </div>
-                    </div>
 
-                    <div class="form-group">
-                        <label for="chart_of_account_id" class="col-sm-3 control-label">Chart of Account</label>
-                            <div class="col-sm-9">
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <label for="chart_of_account_id" class="control-label">Chart of Account</label>
                                 <select class="form-control" id="chart_of_account_id" name="chart_of_account_id">
                                     <option value="">Select chart of account</option>
                                     @php
@@ -112,54 +107,60 @@
                                     @endforeach
                                 </select>
                             </div>
-                    </div>
-
-                    <div class="form-group">
-                        <label for="ref" class="col-sm-3 control-label">Reference</label>
-                        <div class="col-sm-9">
-                            <input type="text" name="ref" class="form-control " id="ref">
                         </div>
                     </div>
 
-                    <div class="form-group">
-                        <label for="transaction_type" class="col-sm-3 control-label">Transaction Type</label>
-                        <div class="col-sm-9">
-                            <select class="form-control" id="transaction_type" name="transaction_type">
-                                <option value="">Select transaction type</option>
-                                <option value="Received">Received</option>
-                                <option value="Payment">Payment</option>
-                            </select>
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <label for="ref" class="control-label">Reference</label>
+                                <input type="text" name="ref" class="form-control " id="ref">
+                            </div>
+                        </div>
+
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <label for="transaction_type" class="control-label">Transaction Type</label>
+                                <select class="form-control" id="transaction_type" name="transaction_type">
+                                    <option value="">Select transaction type</option>
+                                    <option value="Received">Received</option>
+                                    <option value="Payment">Payment</option>
+                                </select>
+                            </div>
                         </div>
                     </div>
 
-                    <div class="form-group">
-                        <label for="amount" class="col-sm-3 control-label">Amount</label>
-                        <div class="col-sm-9">
-                            <input type="text" name="amount" class="form-control " id="amount">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <label for="amount" class="control-label">Amount</label>
+                                <input type="text" name="amount" class="form-control " id="amount">
+                            </div>
                         </div>
-                    </div>
 
-                    <div class="form-group" style="display: none">
-                        <label for="at_amount" class="col-sm-3 control-label">Total Amount</label>
-                        <div class="col-sm-9">
-                            <input type="text" name="at_amount" class="form-control " id="at_amount">
-                        </div>
-                    </div>
-
-                    <div class="form-group" id="payment_type_container">
-                        <label for="payment_type" class="col-sm-3 control-label">Payment Type</label>
-                        <div class="col-sm-9">
-                            <select class="form-control" id="payment_type" name="payment_type">
-                                <option value="">Select payment type</option>
-                                <option value="Cash">Cash</option>
-                                <option value="Bank">Bank</option>
-                            </select>
-                        </div>
-                    </div>
-
-                    <div class="form-group">
-                        <label for="share_holder_id" class="col-sm-3 control-label">Share Holder</label>
+                        <div class="form-group" style="display: none">
+                            <label for="at_amount" class="col-sm-3 control-label">Total Amount</label>
                             <div class="col-sm-9">
+                                <input type="text" name="at_amount" class="form-control " id="at_amount">
+                            </div>
+                        </div>
+
+                        <div class="col-md-6">
+                            <div class="form-group" id="payment_type_container">
+                                <label for="payment_type" class="control-label">Payment Type</label>
+                                <select class="form-control" id="payment_type" name="payment_type">
+                                    <option value="">Select payment type</option>
+                                    <option value="Cash">Cash</option>
+                                    <option value="Bank">Bank</option>
+                                </select>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <label for="share_holder_id" class="control-label">Share Holder</label>
                                 <select class="form-control" id="share_holder_id" name="share_holder_id">
                                     <option value="">Select share holder</option>
                                     @foreach($equityHolders as $equityHolder)
@@ -167,12 +168,13 @@
                                     @endforeach
                                 </select>
                             </div>
-                    </div>
+                        </div>
 
-                    <div class="form-group">
-                        <label for="description" class="col-sm-3 control-label">Description</label>
-                        <div class="col-sm-9">
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <label for="description" class="control-label">Description</label>
                                 <textarea class="form-control" id="description" rows="3" placeholder="Description" name="description"></textarea>
+                            </div>
                         </div>
                     </div>
 
@@ -298,9 +300,15 @@
                 // console.log(response);
                 if (response.status === 200) {
                     $('#chartModal').modal('toggle');
-                    showSnakBar(response.message);
+                    swal({
+                        text: "Saved successfully",
+                        icon: "success",
+                        button: {
+                            text: "OK",
+                            className: "swal-button--confirm"
+                        }
+                    });
                     customerTBL.draw();
-                    $('#alert-container').html('');
                 } else if (response.status === 303) {
                     let alertMessage = `<div class='alert alert-warning'><a href='#' class='close' data-dismiss='alert' aria-label='close'>&times;</a><b>${response.message}</b></div>`;
                     $('#alert-container1').html(alertMessage);
@@ -328,9 +336,15 @@
             success: function (response) {
                 if (response.status === 200) {
                     $('#chartModal').modal('toggle');
-                    showSnakBar(response.message);
+                    swal({
+                        text: "Updated successfully",
+                        icon: "success",
+                        button: {
+                            text: "OK",
+                            className: "swal-button--confirm"
+                        }
+                    });
                     customerTBL.draw();
-                    $('#alert-container').html('');
                 } else if (response.status === 303) {
                     let alertMessage = `<div class='alert alert-warning'><a href='#' class='close' data-dismiss='alert' aria-label='close'>&times;</a><b>${response.message}</b></div>`;
                     $('#alert-container1').html(alertMessage);
